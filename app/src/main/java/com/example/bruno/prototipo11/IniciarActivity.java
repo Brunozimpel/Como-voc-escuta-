961f1b20a47cd341bd650c997c96396205aad16a
GLOFonseca <guilofonseca@gmail.com>
2016-05-18 18:51:40 -0300

    IniciarActivity1.0 activity_iniciar1.0

---
+++ app/src/main/java/com/example/bruno/prototipo11/IniciarActivity.java
@@ -0,0 +1,11 @@
+package com.example.bruno.prototipo11;
+
+import android.app.Activity;
+
+/**
+ * Created by Guilherme on 18/05/16.
+ */
+
+// Activity para Iniciar a Calibração ou o Teste
+public class IniciarActivity extends Activity {
+}

67832932ed1ffc3c533391395c9d4ad45832a216
GLOFonseca <guilofonseca@gmail.com>
2016-05-18 19:07:39 -0300

    IniciarActivity

--- app/src/main/java/com/example/bruno/prototipo11/IniciarActivity.java
+++ app/src/main/java/com/example/bruno/prototipo11/IniciarActivity.java
@@ -1,6 +1,7 @@
 package com.example.bruno.prototipo11;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 /**
  * Created by Guilherme on 18/05/16.
@@ -8,4 +9,10 @@ import android.app.Activity;
 
 // Activity para Iniciar a Calibração ou o Teste
 public class IniciarActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_iniciar);
+    }
 }

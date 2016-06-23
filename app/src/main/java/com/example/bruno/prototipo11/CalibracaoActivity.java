ccbff03c62cef40989c31fa17137b529919e87a5
Brunozimpel <brunozimpel@gmail.com>
2016-06-22 22:42:38 -0300

    Autalizacao2.1 -- atualizei os nomes dos texts views pra mandar os resutlados do esquerdo em outra atividade

--- app/src/main/java/com/example/bruno/prototipo11/CalibracaoActivity.java
+++ app/src/main/java/com/example/bruno/prototipo11/CalibracaoActivity.java
@@ -1,267 +1,95 @@
 package com.example.bruno.prototipo11;
 
 
-import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
-import android.widget.LinearLayout;
+
+
+
+
+import android.content.Intent;
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
+import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.os.Environment;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.content.Context;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.media.MediaRecorder;
-import android.media.MediaPlayer;
 
 import java.io.IOException;
 
 
-public class CalibracaoActivity extends AppCompatActivity
-{
-    private static final String LOG_TAG = "AudioRecordTest";
-    private static String mFileName = null;
+/**
+ * Created by bruno on 18/05/2016.
+ */
+public class CalibracaoActivity extends AppCompatActivity {
 
-    private RecordButton mRecordButton = null;
-    private MediaRecorder mRecorder = null;
+    private final int duration = 2; // seconds
+    private final int sampleRate = 4000;
+    private final int numSamples = duration * sampleRate;
+    private final double sample[] = new double[numSamples];
+    private final double freqOfTone = 1000; // hz
 
-    private PlayButton   mPlayButton = null;
-    private MediaPlayer   mPlayer = null;
+    private final byte generatedSnd[] = new byte[2 * numSamples];
 
-    private void onRecord(boolean start) {
-        if (start) {
-            startRecording();
-        } else {
-            stopRecording();
-        }
-    }
 
-    private void onPlay(boolean start) {
-        if (start) {
-            startPlaying();
-        } else {
-            stopPlaying();
-        }
-    }
 
-    private void startPlaying() {
-        mPlayer = new MediaPlayer();
-        try {
-            mPlayer.setDataSource(mFileName);
-            mPlayer.prepare();
-            mPlayer.start();
-        } catch (IOException e) {
-            Log.e(LOG_TAG, "prepare() failed");
-        }
-    }
-
-    private void stopPlaying() {
-        mPlayer.release();
-        mPlayer = null;
-    }
-
-    private void startRecording() {
-        mRecorder = new MediaRecorder();
-        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-        mRecorder.setOutputFile(mFileName);
-        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-
-        try {
-            mRecorder.prepare();
-        } catch (IOException e) {
-            Log.e(LOG_TAG, "prepare() failed");
-        }
-
-        mRecorder.start();
-    }
-
-    private void stopRecording() {
-        mRecorder.stop();
-        mRecorder.release();
-        mRecorder = null;
-    }
-
-    class RecordButton extends Button {
-        boolean mStartRecording = true;
-
-        OnClickListener clicker = new OnClickListener() {
-            public void onClick(View v) {
-                onRecord(mStartRecording);
-                if (mStartRecording) {
-                    setText("Stop recording");
-                } else {
-                    setText("Start recording");
-                }
-                mStartRecording = !mStartRecording;
-            }
-        };
-
-        public RecordButton(Context ctx) {
-            super(ctx);
-            setText("Start recording");
-            setOnClickListener(clicker);
-        }
-    }
-
-    class PlayButton extends Button {
-        boolean mStartPlaying = true;
-
-        OnClickListener clicker = new OnClickListener() {
-            public void onClick(View v) {
-                onPlay(mStartPlaying);
-                if (mStartPlaying) {
-                    setText("Stop playing");
-                } else {
-                    setText("Start playing");
-                }
-                mStartPlaying = !mStartPlaying;
-            }
-        };
-
-        public PlayButton(Context ctx) {
-            super(ctx);
-            setText("Start playing");
-            setOnClickListener(clicker);
-        }
-    }
-
-    public CalibracaoActivity() {
-        mFileName = Environment.getExternalStorageDirectory().getAbsolutePath();
-        mFileName += "/audiorecordtest.3gp";
-    }
+    Handler handler = new Handler();
 
     @Override
-    public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calibracao);
 
-        LinearLayout ll = new LinearLayout(this);
-        mRecordButton = new RecordButton(this);
-        ll.addView(mRecordButton,
-                new LinearLayout.LayoutParams(
-                        ViewGroup.LayoutParams.WRAP_CONTENT,
-                        ViewGroup.LayoutParams.WRAP_CONTENT,
-                        0));
-        mPlayButton = new PlayButton(this);
-        ll.addView(mPlayButton,
-                new LinearLayout.LayoutParams(
-                        ViewGroup.LayoutParams.WRAP_CONTENT,
-                        ViewGroup.LayoutParams.WRAP_CONTENT,
-                        0));
-        setContentView(ll);
-    }
+
 
     @Override
-    public void onPause() {
-        super.onPause();
-        if (mRecorder != null) {
-            mRecorder.release();
-            mRecorder = null;
+    protected void onResume() {
+        super.onResume();
+        // Use a new tread as this can take a while
+        final Thread thread = new Thread(new Runnable() {
+            public void run() {
+                genTone();
+                handler.post(new Runnable() {
+
+                    public void run() {
+                        playSound();
+
+                    }
+                });
+            }
+        });
+        thread.start();
+    }
+
+
+    void genTone() {
+        // fill out the array
+        for (int i = 0; i < numSamples; ++i) {
+            sample[i] = Math.sin(2 * Math.PI * i / (sampleRate / freqOfTone));
         }
 
-        if (mPlayer != null) {
-            mPlayer.release();
-            mPlayer = null;
+        // convert to 16 bit pcm sound array
+        // assumes the sample buffer is normalised.
+        int idx = 0;
+        for (final double dVal : sample) {
+            // scale to maximum amplitude
+            final short val = (short) ((dVal * 32767));
+            // in 16 bit wav PCM, first byte is the low order byte
+            generatedSnd[idx++] = (byte) (val & 0x00ff);
+            generatedSnd[idx++] = (byte) ((val & 0xff00) >>> 8);
+
         }
     }
+
+    void playSound() {
+        final AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
+                sampleRate, AudioFormat.CHANNEL_OUT_MONO,
+                AudioFormat.ENCODING_PCM_16BIT, generatedSnd.length,
+                AudioTrack.MODE_STATIC);
+        audioTrack.write(generatedSnd, 0, generatedSnd.length);
+        audioTrack.play();
+    }
+
 }
-
-
-
-
-
-
-
-
-
-
-
-//import android.content.Intent;
-//import android.media.AudioFormat;
-//import android.media.AudioManager;
-//import android.media.AudioTrack;
-//import android.media.MediaRecorder;
-//import android.os.Bundle;
-//import android.os.Environment;
-//import android.os.Handler;
-//import android.os.SystemClock;
-//import android.support.v7.app.AppCompatActivity;
-//import android.util.Log;
-//
-//import java.io.IOException;
-//
-//
-///**
-// * Created by bruno on 18/05/2016.
-// */
-//public class CalibracaoActivity extends AppCompatActivity {
-//
-//    private final int duration = 2; // seconds
-//    private final int sampleRate = 4000;
-//    private final int numSamples = duration * sampleRate;
-//    private final double sample[] = new double[numSamples];
-//    private final double freqOfTone = 1000; // hz
-//
-//    private final byte generatedSnd[] = new byte[2 * numSamples];
-//
-//
-//
-//    Handler handler = new Handler();
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_calibracao);
-//
-//
-//
-//    @Override
-//    protected void onResume() {
-//        super.onResume();
-//        // Use a new tread as this can take a while
-//        final Thread thread = new Thread(new Runnable() {
-//            public void run() {
-//                genTone();
-//                handler.post(new Runnable() {
-//
-//                    public void run() {
-//                        playSound();
-//
-//                    }
-//                });
-//            }
-//        });
-//        thread.start();
-//    }
-//
-//
-//    void genTone() {
-//        // fill out the array
-//        for (int i = 0; i < numSamples; ++i) {
-//            sample[i] = Math.sin(2 * Math.PI * i / (sampleRate / freqOfTone));
-//        }
-//
-//        // convert to 16 bit pcm sound array
-//        // assumes the sample buffer is normalised.
-//        int idx = 0;
-//        for (final double dVal : sample) {
-//            // scale to maximum amplitude
-//            final short val = (short) ((dVal * 32767));
-//            // in 16 bit wav PCM, first byte is the low order byte
-//            generatedSnd[idx++] = (byte) (val & 0x00ff);
-//            generatedSnd[idx++] = (byte) ((val & 0xff00) >>> 8);
-//
-//        }
-//    }
-//
-//    void playSound() {
-//        final AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
-//                sampleRate, AudioFormat.CHANNEL_OUT_MONO,
-//                AudioFormat.ENCODING_PCM_16BIT, generatedSnd.length,
-//                AudioTrack.MODE_STATIC);
-//        audioTrack.write(generatedSnd, 0, generatedSnd.length);
-//        audioTrack.play();
-//    }
-//
-//}
